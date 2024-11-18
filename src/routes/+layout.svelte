01b356608124b1adbea49f3092506973bdaa1350
Luis Alvarez <elringo70@hotmail.com>
2024-11-17 21:01:18 -0600

    first commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
 	import '../app.css';
+   import * as Menubar from "$lib/components/ui/menubar/index.js";
+  
 	let { children } = $props();
 </script>
 
+<Menubar.Root>
+  <Menubar.Menu>
+    <Menubar.Trigger>File</Menubar.Trigger>
+    <Menubar.Content>
+      <Menubar.Item>
+        New Tab
+        <Menubar.Shortcut>⌘T</Menubar.Shortcut>
+      </Menubar.Item>
+      <Menubar.Item>New Window</Menubar.Item>
+      <Menubar.Separator />
+      <Menubar.Item>Share</Menubar.Item>
+      <Menubar.Separator />
+      <Menubar.Item>Print</Menubar.Item>
+    </Menubar.Content>
+  </Menubar.Menu>
+</Menubar.Root>
 {@render children()}
